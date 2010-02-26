f667eaf1b71bd571bc7c4e75b86ac9ed6df61e75
Mark S <mark@mark-s.net>
2010-02-26 10:34:40 -0500

    back to .gems

--- app.rb
+++ app.rb
@@ -1,22 +1,19 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'vendor', 'gems', 'environment'))
-Bundler.require_env
+%w(rubygems sinatra rest_client json).each {|lib| require lib}
 
-class MyApp < Sinatra::Base
-  get '/' do
-    return_string = "<h2>Woops</h2>"
-       return_string += "<p>I think you wanted this route: (examples below)</p>"
-       return_string += "<pre>/call/TO/NETWORK/MESSAGE</pre><ul>"
-       examples = ["/call/AIM_USER/aim/Hi AIM user","/call/PHONE_NUMBER/voice/Hi. I just rang your phone.","/call/CELL_PHONE_NUMBER/sms/Hello Moto"]
-       examples.each{|ex| return_string += "<li><a href='#{ex}'>#{ex}</a></li>"}
-       return_string += "</ul>"
-     return_string
-  end
+get '/' do
+  return_string = "<h2>Woops</h2>"
+     return_string += "<p>I think you wanted this route: (examples below)</p>"
+     return_string += "<pre>/call/TO/NETWORK/MESSAGE</pre><ul>"
+     examples = ["/call/AIM_USER/aim/Hi AIM user","/call/PHONE_NUMBER/voice/Hi. I just rang your phone.","/call/CELL_PHONE_NUMBER/sms/Hello Moto"]
+     examples.each{|ex| return_string += "<li><a href='#{ex}'>#{ex}</a></li>"}
+     return_string += "</ul>"
+   return_string
+end
 
-  # get '/call/:to/:network/:message' do
-  #   return new_session(params,config)
-  # end
+# get '/call/:to/:network/:message' do
+#   return new_session(params,config)
+# end
 
-  get '/cvs' do
-    "Not implemented yet!"
-  end
+get '/cvs' do
+  "Not implemented yet!"
 end
