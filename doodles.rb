20597d2e20c9bfdb26473e8252d44cda8b6168c2
Mark S <mark@mark-s.net>
2010-03-12 19:22:28 -0500

    removed doodles.rb

--- doodles.rb
+++
@@ -1,6 +0,0 @@
-# http://api.tropo.com/1.0/sessions?action=create&token=e360244f4526cf439418f7bc71500f09c6c1e688c65d08a38866f913d36991432ce4d7f609d94be7c82f6acd&to=SkramX&network=aim&channel=text&message=Hi+AIM+user
-require 'rubygems'
-require 'pp'
-require 'rest_client'
-response = RestClient.get("http://api.tropo.com/1.0/sessions?action=create&token=e360244f4526cf439418f7bc71500f09c6c1e688c65d08a38866f913d36991432ce4d7f609d94be7c82f6acd&to=SkramX&network=aim&channel=text&message=Hi+AIM+user",:content_type => :json, :accept => :json)
-pp response
